fe4e5dde150a1616f8fe534696758048bcdc3b6f
TUYISENGE Anabella <bellaxbx1109@gmail.com>
2018-12-21 16:20:16 +0200

    remove debugging tools

--- js/scripts.js
+++ js/scripts.js
@@ -107,7 +107,7 @@ $(document).ready(function(){
       correctionOfQuiz(answer1,answer2,answer3,answer4,answer5,answer6,answer7,answer8,answer9,answer10,answer11,answer12,answer13,answer14,answer15,answer16,answer17,answer18,answer19,answer20);
     
 
- console.log(finalGrade)
+ 
   $("#phrase").show();
   display(finalGrade);
   $("form#quiz").hide();
